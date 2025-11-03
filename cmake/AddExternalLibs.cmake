f6721fa378c6cacd04b1544837f55191c5bf9acb
Moritz Schmidt <mo.schmidt1007@gmail.com>
2025-11-03 07:06:01 +0100

    added boost and multiprecision to dependencies

--- cmake/AddExternalLibs.cmake
+++ cmake/AddExternalLibs.cmake
@@ -1,5 +1,6 @@
 include(FetchContent)
 
+# json handling
 FetchContent_Declare(
         nlohmann_json
         GIT_REPOSITORY https://github.com/nlohmann/json
@@ -7,6 +8,7 @@ FetchContent_Declare(
         GIT_SHALLOW TRUE)
 FetchContent_MakeAvailable(nlohmann_json)
 
+# better logging
 FetchContent_Declare(
         spdlog
         GIT_REPOSITORY https://github.com/gabime/spdlog
@@ -14,6 +16,7 @@ FetchContent_Declare(
         GIT_SHALLOW TRUE)
 FetchContent_MakeAvailable(spdlog)
 
+# Cli tools
 FetchContent_Declare(
         cxxopts
         GIT_REPOSITORY https://github.com/jarro2783/cxxopts
@@ -21,6 +24,33 @@ FetchContent_Declare(
         GIT_SHALLOW TRUE)
 FetchContent_MakeAvailable(cxxopts)
 
+# Boost for more accuracy and bigger floats
+FetchContent_Declare(
+  multiprecision
+  GIT_REPOSITORY https://github.com/boostorg/multiprecision.git
+  GIT_TAG      Boost_1_86_0
+  GIT_SHALLOW TRUE
+)
+FetchContent_MakeAvailable(multiprecision)
+
+FetchContent_Declare(
+  math
+  GIT_REPOSITORY https://github.com/boostorg/math.git
+  GIT_TAG      boost-1.86.0
+  GIT_SHALLOW TRUE
+)
+FetchContent_MakeAvailable(math)
+
+
+# GUI
+FetchContent_Declare(
+        fltk
+        GIT_REPOSITORY
+        GIT_TAG release-1.4.4
+        GIT_SHALLOW TRUE)
+FetContent_MakeAvailable(fltk)
+
+# Testing
 if(ENABLE_TESTING)
 	FetchContent_Declare(
 		Catch2
